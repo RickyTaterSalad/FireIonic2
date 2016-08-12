c292cf93f926160be8b55d849260daf66740e0db
RickyTaterSalad <rich5127@esri.com>
2016-08-12 13:25:30 -0700

    cleaned up promises

--- app/providers/department-data.ts
+++ app/providers/department-data.ts
@@ -16,6 +16,7 @@ export class DepartmentData {
   constructor(private af:AngularFire) {
     this.storage = new Storage(LocalStorage);
   }
+
   //resolves true if the key exists in firebase
   private KeyExists:Function = function (key:string) {
     return new Promise((resolve) => {
@@ -28,29 +29,22 @@ export class DepartmentData {
   };
   //writes the current shifts object to the users storage
   private WriteCurrentShiftsToStorage:Function = function () {
-    return new Promise((resolve) => {
-      //write the shifts to storage
-      if (this.shifts) {
-        try {
-          var shiftsAsJson = JSON.stringify(this.shifts);
-          this.storage.set(this.laCityFireKey + "_shifts", shiftsAsJson).then(()=> {
-            resolve()
-          });
-        }
-        catch (e) {
-          resolve();
-        }
+    //write the shifts to storage
+    if (this.shifts) {
+      try {
+        var shiftsAsJson = JSON.stringify(this.shifts);
+        this.storage.set(this.laCityFireKey + "_shifts", shiftsAsJson);
       }
-      else {
-        resolve();
+      catch (e) {
       }
-    })
+    }
   };
   //called when the users client side calendar does not contain the current year that the calendar is requesting
   private AddShiftYear:Function = function (year:number, shiftsToMerge:Array<Object>) {
     return new Promise((resolve) => {
       if (!shiftsToMerge) {
         resolve();
+        return;
       }
       this.storage.get(this.laCityFireKey + "_shifts").then((storedShifts)=> {
         if (storedShifts) {
@@ -58,114 +52,100 @@ export class DepartmentData {
             let shiftsObject = JSON.parse(storedShifts);
             shiftsObject[year] = shiftsToMerge;
             this.shifts = shiftsObject;
-            this.WriteCurrentShiftsToStorage().then(()=> {
-              resolve()
-            });
+            this.WriteCurrentShiftsToStorage();
+            resolve();
+            return;
           }
           catch (e) {
           }
         }
-        else {
-          this.shifts = [];
-          this.shifts[year] = shiftsToMerge;
-          this.WriteCurrentShiftsToStorage().then(()=> {
-            resolve()
-          });
+        //failed to read stored shifts. just write the current year from this response and persist.
+        this.shifts = [];
+        this.shifts[year] = shiftsToMerge;
+        try {
+          this.WriteCurrentShiftsToStorage();
         }
+        catch (e) {
+        }
+        resolve()
       });
     });
   };
   //loads shifts from firebase and stores them in local storage.
   public LoadShiftsFromServer:Function = function () {
     return new Promise((resolve) => {
-      var deptKey = "/departments/" + this.laCityFireKey + "/shifts";
-      console.log(deptKey);
-      //make sure the key exists
-      this.KeyExists(deptKey).then((exists)=> {
-        if (exists) {
+        var deptKey = "/departments/" + this.laCityFireKey + "/shifts";
+        console.log(deptKey);
+        //make sure the key exists
+        this.KeyExists(deptKey).then((exists)=> {
+          if (!exists) {
+            resolve();
+            return;
+          }
           console.log("Loading shifts from server");
           //it exists, load from server
           var sub = this.af.database.object(deptKey).subscribe((dat) => {
             sub.unsubscribe();
             if (dat) {
-              try {
-                this.shifts = dat;
-                //write to storage then resolve
-                this.WriteCurrentShiftsToStorage().then(()=> {
-                  resolve()
-                });
-              }
-              catch (e) {
-                resolve();
-              }
+              this.shifts = dat;
+              //write to storage then resolve
+              this.WriteCurrentShiftsToStorage();
             }
-            else {
-              resolve();
-            }
-
+            resolve();
           });
-        }
-        else {
-          resolve();
-        }
-      });
-    });
+        });
+      }
+    );
   };
-  //called when the year doesn't exist in the users client side calendar object
+//called when the year doesn't exist in the users client side calendar object
   public PopulateShiftsForYear:Function = function (year:number):Promise<Array<Object>> {
     let deptKey = "/departments/" + this.laCityFireKey + "/shifts/" + year;
     return new Promise((resolve) => {
       this.KeyExists(deptKey).then((exists) => {
-        if (exists) {
-          console.log(deptKey);
-          var sub = this.af.database.object(deptKey).subscribe((shiftsForYear) => {
-            console.log("shifts for year:");
-            console.dir(shiftsForYear);
-            sub.unsubscribe();
-            if (shiftsForYear) {
-              this.AddShiftYear(year, shiftsForYear).then(()=> {
-                resolve(this.shifts)
-              });
-            }
-            else {
-              resolve(this.shifts)
-            }
-          });
-        }
-        else {
+        if (!exists) {
           resolve(this.shifts);
+          return;
         }
-      })
-    })
-  };
-  //returns shifts from either local storage or firebase. if loaded from firebase it will store the users client side object
-  public RetrieveDepartmentShifts:Function = function ():Promise<Array<Object>> {
-    //will eventually read the la city key off the users object
-    return new Promise((resolve) => {
-      this.storage.get(this.laCityFireKey + "_shifts").then((shifts)=> {
-        if (shifts) {
-          console.log("Found shifts in storage");
-          try {
-            this.shifts = JSON.parse(shifts);
-          }
-          catch (e) {
-          }
-          if (!this.shifts) {
-            this.LoadShiftsFromServer().then(()=> {
+        console.log(deptKey);
+        var sub = this.af.database.object(deptKey).subscribe((shiftsForYear) => {
+          sub.unsubscribe();
+          if (shiftsForYear) {
+            this.AddShiftYear(year, shiftsForYear).then(()=> {
               resolve(this.shifts)
             });
           }
           else {
             resolve(this.shifts)
           }
-        }
-        else {
+        });
+      })
+    })
+  };
+//returns shifts from either local storage or firebase. if loaded from firebase it will store the users client side object
+  public RetrieveDepartmentShifts:Function = function ():Promise<Array<Object>> {
+    //will eventually read the la city key off the users object
+    return new Promise((resolve) => {
+      this.storage.get(this.laCityFireKey + "_shifts").then((shifts)=> {
+        if (!shifts) {
           this.LoadShiftsFromServer().then(()=> {
             resolve(this.shifts)
           });
+          return;
         }
+        console.log("Found shifts in storage");
+        try {
+          this.shifts = JSON.parse(shifts);
+          resolve(this.shifts);
+          return;
+        }
+        catch (e) {
+        }
+        //is parsing json failed try to load from server
+        this.LoadShiftsFromServer().then(()=> {
+          resolve(this.shifts)
+        });
       });
     });
-  };
+  }
 }
 
